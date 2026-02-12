17dc7bbf1d5da168b94023990d274d271e0fe076
JesusTiburzio <176175797+JTibu9@users.noreply.github.com>
2026-02-11 19:39:26 -0600

    Refactor CANSAT registration GUI and logic
    IMPROVED VERSION

--- TAREAS/TAREA2.py
+++ TAREAS/TAREA2.py
@@ -1,51 +1,121 @@
 import tkinter as tk
-from tkinter import ttk
-from tkinter import messagebox
-
-from reportlab.graphics.samples.excelcolors import backgroundGrey
-
-ventana = tk.Tk()
-ventana.Label("CALCULADORA DE PROMEDIOS",font=("Arial", 12),).pack(pady=10)
-ventana.geometry("600x600")
-ventana.resizable(width=False, height=False)
+from tkinter import ttk, messagebox, Radiobutton
 
 
+def register():
+    team= entry_equipo.get().strip()
+    integrante= entry_integrante.get().strip()
+    categoria = categoria_var.get()
+    micro = micro_var.get()
+    rol = rol_var.get()
+    reglas = reglas_var.get()
+    materiales= materiales_var.get()
+
+    if not team:
+        messagebox.showerror("ERROR","Error, El nombre del equipo es obligatorio")
+        return
+    if not integrante:
+        messagebox.showerror("ERROR","Error, El nombre del integrante es obligatorio")
+        return
+    if not micro:
+        messagebox.showerror("ERROR","Error, El nombre del microcontrolador a usar es obligatorio")
+        return
+    if not reglas:
+        messagebox.showerror("No se haga wei","Acepte las reglas antes de registrarse")
+        return
+    reglas = "si" if reglas else "no"
+    materiales= "si" if materiales else "no"
+
+    print("=====REGISTRO CANSAT 2026=====")
+    print(f"Equipo: {team}")
+    print(f"Categoria: {categoria}")
+    print(f"Microcontrolador: {micro}")
+    print(f"Integrante: {integrante}")
+    print(f"Rol: {rol}")
+    print(f"Materiales: {materiales}")
+    print(f"Reglas: {reglas}")
+
+    messagebox.showinfo("Registrado correctamente", "REGISTRO EXITOSO (づ￣ 3￣)づ")
+
+#VENTANA
+root = tk.Tk()
+root.title("REGISTRO CANSAT 2026")
+root.geometry("400x800")
+root.resizable(False,True)
+
+#APARIENCIAS
+style = ttk.Style()
+style.theme_use("clam")
+
+style.configure("TLabel",font=("Segoe UI",10))
+style.configure("Tittle.TLabel",font=("Segoe UI",10,"bold"))
+style.configure("TButton",font=("Segoe UI",10,"bold"))
+style.configure("Accent.TButton",background="#2E8B57",foreground="white")
+
+#CONTENEDOR PRINCIPAL
+main_frame=ttk.Frame(root,padding=20)
+main_frame.pack(fill="both",expand=True)
+
+ttk.Label(main_frame, text="🚀 REGISTRO CANSAT", style="Title.TLabel").grid(row=0, column=0, columnspan=2, pady=10)
+
+#DATOS
+datos_frame= ttk.LabelFrame(main_frame, text="Datos del Equipo", padding=10)
+datos_frame.grid(row=1,column=0,columnspan=2,sticky="ew",pady=10)
+
+ttk.Label(datos_frame, text="Equipo").grid(row=0,column=0,sticky="w")
+entry_equipo= ttk.Entry(datos_frame, width=30)
+entry_equipo.grid(row=0,column=1,pady=5)
+
+ttk.Label(datos_frame,text="Integrante:").grid(row=1,column=0,sticky="w")
+entry_integrante= ttk.Entry(datos_frame, width= 30)
+entry_integrante.grid(row=1,column=1,pady=5)
 
 
-titulo = tk.Label(ventana, text="CALCULADORA DE PROMEDIOS",font=("Arial", 25))
-titulo.pack(pady=10)
+# SECCION CATEGORIAS
+categoria_frame=ttk.LabelFrame(main_frame,text="Categoria",padding=10)
+categoria_frame.grid(row=2,column=0,columnspan=2,sticky="ew",pady=10)
 
-tk.Label(ventana, text= "Parcial1",padx=10,pady=10).pack(pady=10)
-tk.Label(ventana, text= "Parcial2", padx=10,pady=10).pack(pady=10)
-tk.Label(ventana, text= "Parcial3", pady=10).pack(pady=10)
-tk.Label(ventana, text= "Proyecto", padx=10,pady=10).pack(pady=10)
+categoria_var= tk.StringVar(value="CANSAT BÁSICO")
 
-entry1=tk.Entry(ventana)
-entry2=tk.Entry(ventana)
-entry3=tk.Entry(ventana)
-entry1.pack()
-entry2.pack()
-entry3.pack()
+ttk.Radiobutton(categoria_frame, text="Cansat Basico", variable=categoria_var,value="Cansat Básico").grid(row=0,column=0,sticky="w")
+ttk.Radiobutton(categoria_frame, text= "Cansat Intermedio", variable=categoria_var,value="Cansat Intermedio").grid(row=1,column=0,sticky="w")
+ttk.Radiobutton(categoria_frame, text= "Cansat Avanzado", variable=categoria_var,value="Cansat Avanzado").grid(row=2,column=0,sticky="w")
+
+# SECCION MICROCONTROLADOR
+ttk.Label(main_frame, text="Microcontrolador:").grid(row=3,column=0,columnspan=2,sticky="ew",pady=(8,0))
+
+micro_var = tk.StringVar()
+combo_micro = ttk.Combobox(main_frame, textvariable= micro_var, state="readonly", width=27)
+combo_micro ["values"] = ("ESP32","ESP32-S3","ARDUINO","RASPBERRY PI ZERO 2W", "RASPBERRY PI PICO")
+combo_micro.current(0)
+combo_micro.grid(row=3,column=1,padx=(150,0),pady=(10,0))
+
+# SECCION ROL
+
+rol_frame = ttk.LabelFrame(main_frame, text="ROL DEL INTEGRANTE", padding=10)
+rol_frame.grid(row=4,column=0,columnspan=2,sticky="ew",pady=10)
+
+rol_var = tk.StringVar(value="PROGRAMACION")
+
+ttk.Radiobutton(rol_frame, text="Programación", variable=rol_var, value="Programación").grid(row=0, column=0, sticky="w")
+ttk.Radiobutton(rol_frame, text="Electrónica", variable=rol_var, value="Electrónica").grid(row=1, column=0, sticky="w")
+ttk.Radiobutton(rol_frame, text="Diseño", variable=rol_var, value="Diseño").grid(row=2, column=0, sticky="w")
+
+# CONFIRMACIONES DE LLENADO
+confirm_frame= ttk.LabelFrame(main_frame, text="Confirmacion", padding=10)
+confirm_frame.grid(row=5,column=0,columnspan=2,sticky="ew",pady=10)
+
+materiales_var = tk.BooleanVar()
+ttk.Checkbutton(confirm_frame, text="Tiene materiales", variable=materiales_var).grid(row=0,column=0,sticky="w")
+
+reglas_var = tk.BooleanVar()
+ttk.Checkbutton(confirm_frame, text= "ACEPTA REGLAS Y TERMINOS Y CONDICIONES", variable=reglas_var).grid(row=1,column=0,sticky="w")
 
 
-def Calcular_promedio():
-    try:
-        cal1=float(entry1.get())
-        cal2=float(entry2.get())
-        cal3=float(entry3.get())
+# BOTON DE CONFIRMACION
+btn_registrar= ttk.Button(main_frame, text="Registrar",command=register)
+btn_registrar.grid(row=6,column=0,columnspan=2,pady=20,ipadx=40,ipady=5)
 
-        promedio = cal1+cal2+cal3 / (3)
-        resultado_label.config(text=f"Promedio: {promedio:.2f}")
-    except ValueError:
-        messagebox.showerror("Error, datos faltantes")
 
-# Botón
-calcular_btn = tk.Button(ventana, text="Calcular Promedio", command=Calcular_promedio)
-calcular_btn.pack(pady=10)
 
-# Resultado
-resultado_label = tk.Label(ventana, text="Promedio: ", font=("Arial", 12))
-resultado_label.pack(pady=10)
-
-# Ejecutar
-ventana.mainloop()
+root.mainloop()
